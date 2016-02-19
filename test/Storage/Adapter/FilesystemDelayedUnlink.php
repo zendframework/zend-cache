6e2693b6031c157e7f77f3e9d4a82e691113b1d6
Sebastian Kors <s.kors@unico-soft.com>
2016-02-19 11:04:51 +0100

    overload unlink method to simulate large operations
    the unlink method in Zend\Cache\Storage\Adapter namespace is overloaded
    to insert some delay on deletion process.

---
+++ test/Storage/Adapter/FilesystemDelayedUnlink.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Zend\Cache\Storage\Adapter;
+
+function unlink($path, $context = null) {
+    usleep(150000);
+    if ($context) {
+        return \unlink($path, $context);
+    }
+
+    return \unlink($path);
+}

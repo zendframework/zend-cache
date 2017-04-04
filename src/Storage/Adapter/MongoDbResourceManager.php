53752e5d869feaf4e777c2cac01d64323af3e143
Vladimír Náprstek <vladimir.naprstek@i-projekt.cz>
2017-04-04 22:20:31 +0200

    Use MongoDB (the same way as Zend\Session).

--- src/Storage/Adapter/MongoDbResourceManager.php
+++ src/Storage/Adapter/MongoDbResourceManager.php
@@ -9,8 +9,9 @@
 
 namespace Zend\Cache\Storage\Adapter;
 
-use MongoCollection;
-use MongoException;
+use MongoDB\Client as MongoClient;
+use MongoDB\Collection as MongoCollection;
+use MongoDB\Driver\Exception\Exception as MongoException;
 use Zend\Cache\Exception;
 
 class MongoDbResourceManager
@@ -86,23 +87,22 @@ class MongoDbResourceManager
             try {
                 if (! isset($resource['db_instance'])) {
                     if (! isset($resource['client_instance'])) {
-                        $clientClass = version_compare(phpversion('mongo'), '1.3.0', '<') ? 'Mongo' : 'MongoClient';
-                        $resource['client_instance'] = new $clientClass(
+                        $resource['client_instance'] = new MongoClient(
                             isset($resource['server']) ? $resource['server'] : null,
                             isset($resource['connection_options']) ? $resource['connection_options'] : [],
                             isset($resource['driver_options']) ? $resource['driver_options'] : []
                         );
                     }
-
+/*
                     $resource['db_instance'] = $resource['client_instance']->selectDB(
                         isset($resource['db']) ? $resource['db'] : ''
                     );
-                }
-
-                $collection = $resource['db_instance']->selectCollection(
-                    isset($resource['collection']) ? $resource['collection'] : ''
+*/                }
+                $collection = $resource['client_instance']->selectCollection(
+                    isset($resource['db']) ? $resource['db'] : 'zend',
+					isset($resource['collection']) ? $resource['collection'] : 'cache'
                 );
-                $collection->ensureIndex(['key' => 1]);
+				$collection->createIndex(['key' => 1]);
 
                 $this->resources[$id]['collection_instance'] = $collection;
             } catch (MongoException $e) {

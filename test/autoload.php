2e23589315662c0f44eab2269a97440f91d533c1
webimpress <contact@webimpress.com>
2017-10-26 10:22:52 +0100

    Added autoloader for test
    Tests are using integration test from cache/integration-tests which has
    not update PHPUnit version

---
+++ test/autoload.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @see       https://github.com/zendframework/zend-cache for the canonical source repository
+ * @copyright Copyright (c) 2017 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   https://github.com/zendframework/zend-cache/blob/master/LICENSE.md New BSD License
+ */
+
+if (! class_exists(\PHPUnit_Framework_TestCase::class)) {
+    class_alias(\PHPUnit\Framework\TestCase::class, \PHPUnit_Framework_TestCase::class, true);
+}

d19f90e37635ec104191501645c8df07e79d2fe7
Maximilian Bösing <max@boesing.email>
2018-09-10 16:29:23 +0200

    Linked the polyfill classes in their stubs

--- stubs/AdapterPluginManager.php
+++ stubs/AdapterPluginManager.php
@@ -2,8 +2,14 @@
 
 namespace Zend\Cache\Storage;
 
+use Zend\Cache\Storage\AdapterPluginManager\AdapterPluginManagerV2Polyfill;
+use Zend\Cache\Storage\AdapterPluginManager\AdapterPluginManagerV3Polyfill;
 use Zend\ServiceManager\AbstractPluginManager;
 
+/**
+ * @see AdapterPluginManagerV2Polyfill
+ * @see AdapterPluginManagerV3Polyfill
+ */
 class AdapterPluginManager extends AbstractPluginManager
 {
 
--- stubs/PatternPluginManager.php
+++ stubs/PatternPluginManager.php
@@ -2,8 +2,14 @@
 
 namespace Zend\Cache;
 
+use Zend\Cache\PatternPluginManager\PatternPluginManagerV2Polyfill;
+use Zend\Cache\PatternPluginManager\PatternPluginManagerV3Polyfill;
 use Zend\ServiceManager\AbstractPluginManager;
 
+/**
+ * @see PatternPluginManagerV2Polyfill
+ * @see PatternPluginManagerV3Polyfill
+ */
 class PatternPluginManager extends AbstractPluginManager
 {
 

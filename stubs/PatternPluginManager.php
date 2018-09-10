0ddade2b8e503ae6332af20dd77e282312d9d4ff
Maximilian Bösing <max@boesing.email>
2018-09-10 16:41:08 +0200

    Added missing file headers

--- stubs/PatternPluginManager.php
+++ stubs/PatternPluginManager.php
@@ -1,5 +1,11 @@
 <?php
-
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/zf2 for the canonical source repository
+ * @copyright Copyright (c) 2018 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
 namespace Zend\Cache;
 
 use Zend\Cache\PatternPluginManager\PatternPluginManagerV2Polyfill;
